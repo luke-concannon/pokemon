32f6e4b7dff3b7db7ecf7038ba7c59bb534f669d
Luke Concannon <lumirari@Lukes-iMac.local>
2022-04-21 21:56:30 +1200

    d

--- client/api/apiClient.js
+++ client/api/apiClient.js
@@ -10,3 +10,23 @@ export const getPokemon = async () => {
     console.error(error)
   }
 }
+
+export const getPokemonByName = async (name) => {
+  try {
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    console.log(minifyPokemon(response.data),pokemonLowerCase())
+    // console.log(response.data)
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export const getPokemonByType = async (type) => {
+  try {
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${type}`)
+    console.log(minifyPokemon(response.data),pokemonLowerCase())
+    // console.log(response.data)
+  } catch(error) {
+    console.error(error)
+  }
+}
